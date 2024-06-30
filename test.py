4162cea5ec8e0a966036ffc55022ba10b3c52a1c
DustinSun <duongnvh.work@gmail.com>
2024-06-30 15:01:55 +0700

    ...

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-from processor import GRMProcessor
+import requests
+import time
+import argparse
+parser = argparse.ArgumentParser(description="Send a prompt to an endpoint.")
+parser.add_argument("prompt", metavar="mode", type=str)
+args = parser.parse_args()
+start_time = time()
+gen_response = requests.post("http://localhost:8093/generate", data={
+    "prompt": args.prompt,
+}, timeout=600)
 
-grm = GRMProcessor()
-
-grm.process("a dog")
-grm.process("a cat")
-grm.process("a snake")
+print(f"Prompt: {args.prompt}")
+print(f"Response: {gen_response.text}")
+print(f"Time taken: {time() - start_time}")

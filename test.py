97b2a7198673caba3007c8200937a77bb838783b
DustinSun <duongnvh.work@gmail.com>
2024-06-30 15:44:32 +0700

    ...

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 import requests
-import time
 import argparse
 parser = argparse.ArgumentParser(description="Send a prompt to an endpoint.")
 parser.add_argument("prompt", metavar="mode", type=str)
 args = parser.parse_args()
-start_time = time()
 gen_response = requests.post("http://localhost:8093/generate", data={
     "prompt": args.prompt,
 }, timeout=600)
 
 print(f"Prompt: {args.prompt}")
 print(f"Response: {gen_response.text}")
-print(f"Time taken: {time() - start_time}")

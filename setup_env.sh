d2005fab6d8c33196eff9dfd8871db85b374c123
DustinSun <duongnvh.work@gmail.com>
2024-07-02 01:12:13 +0700

    ...

--- setup_env.sh
+++ setup_env.sh
@@ -28,11 +28,9 @@ CUDA_HOME=${CONDA_PREFIX}
 pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu121
 cd third_party/diff-gaussian-rasterization &&  pip install -e .
 cd ..
+cd generative_models &&  pip install -e .
 cd ..
-cd third_party/generative_models &&  pip install -e .
-cd ..
-cd ..
-cd third_party/segmentation_models &&  pip install -e .
+cd segmentation_models &&  pip install -e .
 cd ..
 cd ..
 

cf912d49e4c9747374f51ba6bcb7210c34247d15
DustinSun <duongnvh.work@gmail.com>
2024-08-27 18:20:40 +0700

    ...

--- benchmark_filtered.py
+++ benchmark_filtered.py
@@ -22,7 +22,7 @@ for prompt in prompts:
     total_runs += 1
     print(f"Prompt: {prompt.strip()}")
     try:
-        gen_response = requests.post("http://localhost:8093/generate/", data={
+        gen_response = requests.post("http://localhost:8193/generate/", data={
             "prompt": prompt.strip(),
         }, timeout=600)
         score = float(gen_response.text)
